b0bfd104172cf0f1651e8ae2beb375c5fb648468
Nihal7801 <140273386+Nihal7801@users.noreply.github.com>
2024-08-10 13:30:43 +0000

    conversion of dataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// NOTES :-
+
+//"33" => convert in 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// Convert in string
+
+let someNumber = 30
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
