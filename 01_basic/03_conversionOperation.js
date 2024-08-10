f8edeb15e69ea9a2e7b4836b34b45fdce54b2542
Nihal7801 <140273386+Nihal7801@users.noreply.github.com>
2024-08-10 17:55:07 +0000

    conversion Operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -33,3 +33,43 @@ let InBigint = BigInt(someValue)
 console.log(InBigint);
 console.log(typeof InBigint);
 
+// ***************** Operations ******************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = "Nihal"
+
+let str3 = str1+str2
+console.log(str3);
+
+// some more conversion in string
+
+// console.log(1+"2");
+// console.log(2+"1");
+// console.log("1"+2+2);
+// console.log(2+2+"1");
+// console.log((1+3)*4%2);
+
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+let newValue = ++gameCounter;
+console.log(newValue);
+
+
+
+
+
+
