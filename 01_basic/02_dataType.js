39e71ccb999b1905006779cfbf45cd92ddc56e76
Nihal7801 <140273386+Nihal7801@users.noreply.github.com>
2024-08-10 13:00:52 +0000

    dataTypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code in newer version
+
+// alert(3+3) // we are using node js not browser
+
+// alert(3+3) -> we can access that on browser console ,  because console work under documentation
+
+ console.log(3+3)
+//  code readability should be high
+ console.log('Nihal');
+ 
+//  DATA TYPES
+
+let name = "Nihal"
+let age = 18
+let isLoggedIn = false
+
+//There all are primitive datatype
+
+// number => 2 to power 53
+// string = ""
+// bigint => when your integer number is too big like stock market
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object
+console.log(typeof "nihal");
+console.log(typeof age)
+console.log(typeof null);
+
